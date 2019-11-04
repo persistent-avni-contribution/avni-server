4234f61e7232ff792628c0081d4f2017f5b6ed58
sidtharthan <sidtharthann@samanvayfoundation.org>
2019-11-04 22:51:14 +0530

    #0 | create dbuser and organisation through web api

---
+++ openchs-server-api/src/main/java/org/openchs/web/request/OrganisationRequest.java
@@ -0,0 +1,31 @@
+package org.openchs.web.request;
+
+public class OrganisationRequest extends CHSRequest {
+    private String parentUuid;
+    private String name;
+    private String dbUserName;
+
+    public String getParentUuid() {
+        return parentUuid;
+    }
+
+    public void setParentUuid(String parentUuid) {
+        this.parentUuid = parentUuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDbUserName() {
+        return dbUserName;
+    }
+
+    public void setDbUserName(String dbUserName) {
+        this.dbUserName = dbUserName;
+    }
+}
